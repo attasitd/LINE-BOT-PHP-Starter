764eda7f1c176056aec636a265927d015e6d680c
attasitd <attasitd@hotmail.com>
2017-03-24 17:04:11 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'nKOmWzPp5kyankLtyuVRO55hxfvkaYBjufGrossf5gYtJPNEKd83Z/03wxsxyq7cNB1tDckxMJ1Owy2DbxBMFM8+uoR8I6D57tHWVhyCFBPxM3OeaDQ37pWz5A0ONIrTGDXFolIwP+lbLy7JBaXP7gdB04t89/1O/w1cDnyilFU=';
+$access_token = 'wgKVfqXwuHW+tQtWBfy1cIlhxp+AFoyrbBoKo3yrZ6up2lBdy9Du+ZAkzXKnDR1FNB1tDckxMJ1Owy2DbxBMFM8+uoR8I6D57tHWVhyCFBMPG6QV+sejtyV2nJZM+WlXf1ljEVNyWYOmli1g8uPG3QdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
